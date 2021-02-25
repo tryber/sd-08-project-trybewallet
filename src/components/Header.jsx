4dc314849877cb7c853c5091e391f9a48ade2914
MarceloBorges <melombm@hotmail.com>
2021-02-25 17:55:24 -0500

    chamando o componente header no wallet / criando os conteudo no header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <div />
+      <section>
+        <h2>carteira</h2>
+        <div data-testid="email-field" />
+        <div data-testid="total-field">
+          0
+        </div>
+        <div data-testid="header-currency-field">
+          BRL
+        </div>
+
+      </section>
     );
   }
 }
