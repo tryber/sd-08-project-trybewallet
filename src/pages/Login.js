9b5a61a00d64164dfcbee52b273db69b7e30d6d3
gabrieldeori <deori.pessoal@gmail.com>
2021-02-25 18:27:44 -0300

    implemented validations functions

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,34 @@ import logo from '../svg/045-wallet.svg';
 import './Login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputEmail: '',
+      passLength: 0,
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.checkInputs = this.checkInputs.bind(this);
+  }
+
+  handleInput(key, value) {
+    this.setState({
+      [key]: value,
+    });
+  }
+
+  checkInputs() {
+    const minPassLength = 6;
+    const { inputEmail, passLength } = this.state;
+    const emailBool = /^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(inputEmail);
+    if(passLength >= minPassLength && emailBool) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
+    const { inputEmail } = this.state;
     return (
       <div className="login-page">
         <section>
@@ -14,6 +41,8 @@ class Login extends React.Component {
               name="userEmail"
               id="userEmail"
               data-testid="email-input"
+              value={ inputEmail }
+              onChange={ ({ target: { value } }) => this.handleInput('inputEmail', value) }
             />
           </label>
           <label htmlFor="userPassword">
@@ -23,10 +52,12 @@ class Login extends React.Component {
               name="userPassword"
               id="userPassword"
               data-testid="password-input"
+              onChange={ ({ target: { value } }) => this.handleInput('passLength', value.length) }
             />
           </label>
           <button
             type="button"
+            disabled={ !this.checkInputs() }
           >
             Entrar
           </button>
