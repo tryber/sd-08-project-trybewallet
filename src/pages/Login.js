d28ddbf2ef14c77070061b5a56e829c0a745f6a7
MarceloBorges <melombm@hotmail.com>
2021-02-25 15:34:26 -0500

    criando a rotas das paginas / criando os inputs de senha e email / e criando o botao

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <section>
+        <form>
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            placeholder="Digite seu E-mail"
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            placeholder="Sua senha aqui"
+          />
+          <button type="button">Entrar</button>
+        </form>
+      </section>
+    );
   }
 }
 
