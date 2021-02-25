a1417d3a69ccd4dc96bc2c3cf05095cd2e53f33e
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-02-25 14:54:52 -0300

    primeira subrequisitos 1 2 e 3 do requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 // Coloque aqui suas actions
+export const LOGGIN = 'LOGGIN';
+export const WALLET = 'WALLET';
+
+export const logginAction = (email) => ({
+  type: LOGGIN,
+  email,
+});
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,31 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <form>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="email"
+              name="email"
+              placeholder="digite seu email"
+              data-testid="email-input"
+            />
+          </label>
+          <label htmlFor="password">
+            Senha:
+            <input
+              type="password"
+              name="password"
+              placeholder="digite sua senha"
+              data-testid="password-input"
+            />
+            <button type="button">Entrar</button>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
