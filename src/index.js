f82ee814ce6a4a283eaaa142ea4c8242147c76a6
Leonardo <lecastro16@gmail.com>
2021-02-25 16:25:17 -0300

    iniciando o requisito 1 v0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './store/index';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login as loginAction } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    const { login } = this.props;
+    return (
+      <div className="Login">
+        <section className="login-inputs">
+          <input
+            type="text"
+            onChange={(e) => this.setState({ email: e.target.value })}
+            placeholder="email"
+            data-testid="email-input"
+          />
+          <input
+            type="password"
+            onChange={(e) => this.setState({ password: e.target.value })}
+            placeholder="senha"
+            data-testid="password-input"
+          />
+        </section>
+        <div className="link">
+          <Link to="/carteira">
+            <button onClick={() => login({ email, password })}
+            data-testid="btn-login">Entrar</button>
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  login: (e) => dispatch(loginAction(e)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
