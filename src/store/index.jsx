f63a7a4336ba5e922fe1a5ca5c103c6026e1f337
MarceloBorges <melombm@hotmail.com>
2021-02-25 16:44:10 -0500

    modificado o nome reducer para rootReducer para deixar mais bonito

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,10 +2,10 @@
 // compose => fazer composição com dois middleware ( thunk é devTools)
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import reducer from '../reducers';
+import rootReducer from '../reducers';
 
 const store = createStore(
-  reducer,
+  rootReducer,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
