a73dd03f05139aceb54735dcfda05edb66fc1571
emassaki <massakihirayama@gmail.com>
2021-02-25 16:19:05 -0300

    Requisito 1 - Pagina de login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const EMAIL = 'EMAIL';
+export const WALLET = 'WALLET';
+
+export const emailChange = (payload) => ({
+  type: EMAIL,
+  payload,
+});
+
+export const walletChange = (payload) => ({
+  type: WALLET,
+  payload,
+});
