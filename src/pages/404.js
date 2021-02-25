f2ddef2e555719c009ace47cb76d7113dab7bdc4
adrianWarrior <adrianoforcellini>
2021-02-25 20:48:06 -0300

    criaçao notfound

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className="notFound">
+        <h1> Página Não Encontrada</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
