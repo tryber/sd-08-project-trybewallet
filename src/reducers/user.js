d9790978bdcb743ce175eb448a35ea79ba282387
adrianWarrior <adrianoforcellini>
2021-02-25 19:43:40 -0300

    reducer user

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: 'alguem@email.com',
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'CHANGE_EMAIL':
+    return { ...state, email: action.email };
+  default:
+    return state;
+  }
+}
