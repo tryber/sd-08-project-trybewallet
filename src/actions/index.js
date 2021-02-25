58cb37a3ef75173adee957ad399df63b8e5583ee
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-25 19:43:01 -0300

    feat: added actions user

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+
+export const user = {
+  login: (payload) => ({ type: LOGIN, payload }),
+  logout: () => ({ type: LOGOUT }),
+};
+
+export const Wallet = {};
