7676ea50ba3cac011a17a9d775e9f6878053a4e9
Victor Breno <victor.brns2@gmail.com>
2021-02-25 18:54:36 -0300

    Correçao EsLint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import loginAction from '../actions';
 
