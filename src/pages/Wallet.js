a171f7b8feacaf53343ae7d987350bfcfbaa41ba
adrianWarrior <adrianoforcellini>
2021-02-25 20:49:38 -0300

    atualizaçao da pagina wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import ListaDeGastos from '../components/ListaDeGastos';
+import Cambio from '../components/Cambio';
+import AddGastos from '../components/AddGastos';
 
-class Wallet extends React.Component {
+class Wallet extends Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div className="flex-container">
+        <div className="child1">
+          <Header />
+        </div>
+        <div className="child2">
+          <ListaDeGastos />
+        </div>
+        <div className="child3">
+          <Cambio />
+        </div>
+        <div className="child4">
+          <AddGastos />
+        </div>
+
+      </div>
+    );
   }
 }
 
