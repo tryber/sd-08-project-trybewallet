eac2aed807500ac9c45abdde3bf56b5695670d9c
Fabio Ferreira <fabio35547@gmail.com>
2021-02-25 19:02:37 -0300

    Requisitos 1 e 2

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,18 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { GET_EMAIL } from '../actions';
+
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case GET_EMAIL:
+    console.log('Teste');
+    return { email: action.payload };
+  default:
+    return state;
+  }
+};
+
+export default user;
