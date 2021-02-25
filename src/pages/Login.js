cda48228026cc434d29a75f757d458003e4d676a
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-25 15:29:17 -0300

    cria input de email, senha e botao para entrar no componente Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="login-form-container">
+        <form>
+          <label
+            htmlFor="email"
+          >
+            E-mail
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              required
+            />
+          </label>
+          <label
+            htmlFor="email"
+          >
+            Senha
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              minLength="6"
+              required
+            />
+          </label>
+          <Link to="/wallet">
+            <button
+              type="button"
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
