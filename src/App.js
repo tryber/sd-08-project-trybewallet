22afa7b2f11861acd35216cfd07a464dbb161f08
gabrieldeori <deori.pessoal@gmail.com>
2021-02-25 19:24:26 -0300

    Change location of flaticons div

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Login } />
       </Switch>
+      <div className="link-freepik-best-icons">
+        Icons made by&nbsp;
+        <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+        &nbsp;from&nbsp;
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </div>
     </div>
   );
 }
