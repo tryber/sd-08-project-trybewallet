dcc508b4947b7bb49c5d2d309dfb32eefde7d992
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-02-25 15:32:10 -0300

    criando o store

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,51 @@
+import { connect } from 'react-redux';
 import React from 'react';
+import * as Actions from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.checkEmailAndPassword = this.checkEmailAndPassword.bind(this);
+  }
+
+  handleChange(event) {
+    const {
+      target: { name, value },
+    } = event;
+    this.setState({
+      [name]: value,
+    });
+    this.checkEmailAndPassword();
+  }
+
+  checkEmailAndPassword() {
+    const minimumPasswordSize = 5;
+    const { email, password } = this.state;
+    const re = /\S+@\S+\.\S+/;
+    const isValidEmail = re.test(email);
+    const isValidPassword = password.length >= minimumPasswordSize;
+    if (isValidPassword && isValidEmail) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const { loggin } = this.props;
+    loggin(email);
+  }
+
   render() {
+    const { isDisabled } = this.state;
     return (
       <div>
         <form>
@@ -12,6 +56,7 @@ class Login extends React.Component {
               name="email"
               placeholder="digite seu email"
               data-testid="email-input"
+              onChange={ this.handleChange }
             />
           </label>
           <label htmlFor="password">
@@ -21,8 +66,15 @@ class Login extends React.Component {
               name="password"
               placeholder="digite sua senha"
               data-testid="password-input"
+              onChange={ this.handleChange }
             />
-            <button type="button">Entrar</button>
+            <button
+              type="button"
+              disabled={ isDisabled }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
           </label>
         </form>
       </div>
@@ -30,4 +82,8 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  loggin: (email) => dispatch(Actions.logginAction(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
