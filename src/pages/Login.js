5117ede06d8f89755c230f19d58eda3da19e84ca
Lucas Nascimento - MACOS <lucasmvnascimento@gmail.com>
2021-02-25 16:19:28 -0300

    fix lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { loginAction } from '../actions';
+import loginAction from '../actions';
 
 class Login extends React.Component {
   constructor() {
