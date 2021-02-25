fc0e5782c6c6ce6f8f5b53ec2794a16cae6a26af
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-25 19:58:17 -0300

    One more time

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-// import walletimg from './img/walletimg.png';
+import walletimg from './img/walletimg.png';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <div>
-      {/* <img src={ walletimg } alt="Wallet Logo" /> */}
+      <img src={ walletimg } alt="Wallet Logo" />
       <Switch>
         <Route path="/carteira" component={ Wallet } />
         <Route exact path="/" component={ Login } />
