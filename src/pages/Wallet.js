2d4138cae10da98a8cdb7dd085f94813e7e862b5
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-25 20:43:20 -0300

    implementaçao parcial do formulario de despesas

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,14 +3,57 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  renderForm() {
+    const coins = [
+      'USD',
+      'CAD',
+      'EUR',
+      'GBP',
+      'ARS',
+      'BTC',
+      'LTC',
+      'JPY',
+      'CHF',
+      'AUD',
+      'CNY',
+      'ILS',
+      'ETH',
+      'XRP',
+    ];
+    return (
+      <form>
+        <input type="text" data-testid="value-input" />
+        <input type="text" data-testid="description-input" />
+        <select>
+          { coins
+            .map((coin, index) => (
+              <option
+                data-testid={ `${coin}` }
+                key={ index + 1 }
+              >
+                {coin}
+              </option>))}
+        </select>
+      </form>
+    );
+  }
+
   render() {
     const { email } = this.props;
     return (
-      <header className="wallet-header">
-        <span data-testid="email-field">{ email }</span>
-        <span data-testid="total-field">0</span>
-        <span data-testid="header-currency-field">BRL</span>
-      </header>
+      <>
+        <header className="wallet-header">
+          <span data-testid="email-field">{ email }</span>
+          <span data-testid="total-field">0</span>
+          <span data-testid="header-currency-field">BRL</span>
+        </header>
+        { this.renderForm() }
+      </>
     );
   }
 }
