cb24e219aaafdb4eeb4e9cfe808a70bc84e62bdb
Vitor Nunes <vitornunes200@gmail.com>
2021-02-25 15:24:56 -0300

    Commit Inicial - Vitor Nunes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' component={Login}></Route>
+        <Route path='/carteira' component={Wallet}></Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
