2f1c9c50484e2f1f28ea9795fad8931f7188e7bd
MarceloBorges <melombm@hotmail.com>
2021-02-25 18:06:23 -0500

    criando o mapStateToProps para adquirir o email do store /  conectado o componente com connect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { showEmail } = this.props;
     return (
       <section>
         <h2>carteira</h2>
-        <div data-testid="email-field" />
+        <div data-testid="email-field">
+          {`E-mail: ${showEmail}`}
+        </div>
         <div data-testid="total-field">
           0
         </div>
@@ -18,4 +22,12 @@ class Header extends Component {
   }
 }
 
-export default Header;
+// lendo o store do redux
+const mapStateToProps = (state) => {
+  const { user } = state;
+  return {
+    showEmail: user.email,
+  };
+};
+
+export default connect(mapStateToProps)(Header);
