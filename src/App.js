0dd4176bee8fb821da81b8c9053d064f663bff22
Paulo Simões <paulohbsimoes@gmail.com>
2021-02-25 14:51:19 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import './styles/global.css';
+
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <div>Hello, TrybeWallet!</div>
+    </div>
+  );
 }
 
 export default App;
