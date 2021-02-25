d232c41637e1058633e76c752a7deeb0707e762b
Victor Miwa <victoryukimiwa@hotmail.com>
2021-02-25 18:14:50 -0300

    requisito 2 e 3 finalizados

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <>
+      <header>
+        <p data-testid="email-field">Email: via props</p>
+        <p data-testid="total-field">Despesas totais 0</p>
+        <p data-testid="header-currency-field">Mostrar o cambio BRL/outros</p>
+      </header>
+      </>
+    );
   }
 }
 
