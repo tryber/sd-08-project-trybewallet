fb4a92600a6bd2ad567058cd03cceeadbc86e310
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-02-25 15:18:49 -0300

    Inicio do projeto

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Wallet() {
-  return <div>TrybeWallet</div>;
+  const user = useSelector((state) => state.user);
+  const wallet = useSelector((state) => state.wallet);
+  return (
+    <div className="container">
+      <div data-testid="email-field">{user.email}</div>
+      <div data-testid="total-field">{wallet.expenses || 0}</div>
+      <div data-testid="header-currency-field">{wallet.currencies || 'BRL'}</div>
+    </div>
+  );
 }
