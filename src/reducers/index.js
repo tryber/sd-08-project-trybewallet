763d4c286e216ce4bb90975ed74ee88287c2439c
Silvio Dayube <scarige@gmail.com>
2021-02-25 17:09:22 -0300

    Creates rootReducer placeholder

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,17 @@
 // import user from './user';
 // import wallet from './wallet';
+const initialState = {
+  user: {
+    email: '',
+  },
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
 
+export default function reducer(state = initialState, action) {
+  return state;
+}
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
