cbcc7cc8180e4fdb5080dacce5d3cea1891d52b4
adrianWarrior <adrianoforcellini>
2021-02-25 20:31:27 -0300

    reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,14 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  expenses: {
+    valor: 0,
+  },
+};
+
+export default function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ADD_DESPESA':
+    return { ...state, expenses: action.expenses };
+  default:
+    return state;
+  }
+}
