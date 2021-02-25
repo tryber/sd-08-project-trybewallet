58f98d0b5e9fb3a01e594b8d53ad723203fd240e
MarceloBorges <melombm@hotmail.com>
2021-02-25 16:03:55 -0500

    criando state para armazenar o input email e password /  criando um funcao para capturar os inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,43 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.inputChange = this.inputChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  // entrada de dados dos input do name e value do input
+  inputChange({ target: { name, value } }) {
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <section>
+        <h1>Trybe</h1>
         <form>
           <input
             type="text"
             name="email"
             data-testid="email-input"
             placeholder="Digite seu E-mail"
+            onChange={ this.inputChange }
           />
+          <br />
           <input
             type="password"
             name="password"
             data-testid="password-input"
             placeholder="Sua senha aqui"
+            onChange={ this.inputChange }
           />
+          <br />
           <button type="button">Entrar</button>
         </form>
       </section>
