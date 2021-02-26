40dd6660ea7047d89c1f006be5eaa3af1890d56d
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-26 16:52:21 -0300

    cria a funçao que retorna os codigos das moedas em forma de array para criar o select do formulario ExpensesForm

--- src/components/ExpensesForm.js
+++ src/components/ExpensesForm.js
@@ -4,49 +4,41 @@ class ExpensesForm extends Component {
   constructor() {
     super();
     this.renderForm = this.renderForm.bind(this);
+    this.renderCoinList = this.renderCoinList.bind(this);
+  }
+
+  componentDidMount() {
+    this.renderCoinList();
+  }
+
+  async renderCoinList() {
+    const coinsInfo = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const coinsJson = await coinsInfo.json();
+    console.log(coinsJson);
+    const coinsList = Object.values(coinsJson);
+    const coinsCodeList = coinsList
+      .filter((coin) => coin.codein !== 'BRLT')
+      .map((coin) => coin.code);
+    console.log(coinsCodeList);
   }
 
   renderForm() {
-    const coins = [
-      'USD',
-      'CAD',
-      'EUR',
-      'GBP',
-      'ARS',
-      'BTC',
-      'LTC',
-      'JPY',
-      'CHF',
-      'AUD',
-      'CNY',
-      'ILS',
-      'ETH',
-      'XRP',
-    ];
     return (
-      <form>
-        <input type="text" data-testid="value-input" />
-        <input type="text" data-testid="description-input" />
-        <select>
-          { coins
-            .map((coin, index) => (
-              <option
-                data-testid={ `${coin}` }
-                key={ index + 1 }
-              >
-                {coin}
-              </option>))}
-        </select>
-      </form>
+      <div>
+        <form>
+          <input type="text" data-testid="value-input" />
+          <input type="text" data-testid="description-input" />
+        </form>
+      </div>
     );
   }
 
   render() {
     return (
       <div>
-
+        {this.renderForm()}
       </div>
-    )
+    );
   }
 }
 
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,50 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import ExpensesForm from '../components/ExpensesForm';
 
 class Wallet extends React.Component {
-  constructor() {
-    super();
-    this.renderForm = this.renderForm.bind(this);
-  }
-
-  renderForm() {
-    const coins = [
-      'USD',
-      'CAD',
-      'EUR',
-      'GBP',
-      'ARS',
-      'BTC',
-      'LTC',
-      'JPY',
-      'CHF',
-      'AUD',
-      'CNY',
-      'ILS',
-      'ETH',
-      'XRP',
-    ];
-    return (
-      <form>
-        <input type="text" data-testid="value-input" />
-        <input type="text" data-testid="description-input" />
-        <select>
-          { coins
-            .map((coin, index) => (
-              <option
-                data-testid={ `${coin}` }
-                key={ index + 1 }
-              >
-                {coin}
-              </option>))}
-        </select>
-      </form>
-    );
-  }
-
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <ExpensesForm />
+      </>
     );
   }
 }
