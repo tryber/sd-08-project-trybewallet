da1599813acce49bc0f90d41ee8c48b91c45c6ec
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-26 15:08:42 -0300

    refatora Wallet criando componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <header className="wallet-header">
+        <span data-testid="email-field">{ email }</span>
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+function mapStateToProps(state) {
+  return ({
+    email: state.user.email,
+  });
+}
+
+export default connect(mapStateToProps)(Header);
