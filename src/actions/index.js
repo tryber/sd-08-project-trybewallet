ffcf09b7026eac1607a3fec75127a287adad84a2
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-26 20:08:27 -0300

    feat: added actions expense

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,35 @@
+import { getCurrency } from '../services/api';
+
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
+
+export const ADD_EXPENSE = 'ADD_EXPENSE';
+export const GET_CURRENCIES = 'GET_CURRENCIES';
 
 export const user = {
   login: (payload) => ({ type: LOGIN, payload }),
   logout: () => ({ type: LOGOUT }),
 };
 
-export const Wallet = {};
+export const wallet = {
+  addExpense: (payload) => ({
+    type: ADD_EXPENSE,
+    payload,
+  }),
+  getCurrencies: (payload) => ({
+    type: GET_CURRENCIES,
+    payload,
+  }),
+};
+
+export function getCurrencySymbol() {
+  return async (dispatch) => {
+    try {
+      const response = await getCurrency();
+      console.log(response);
+      dispatch(wallet.getCurrencies());
+    } catch (err) {
+      return err;
+    }
+  };
+}
