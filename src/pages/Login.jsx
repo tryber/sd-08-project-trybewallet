a1100d2ffa58ee75b8f5d4b15e15c9325ccda359
vivianeflowt <vivianeflowt@gmail.com>
2021-02-26 02:46:16 -0300

    up

---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import * as actions from "../actions";
+
+import InputEmail from "../components/InputEmail";
+import InputPassword from "../components/InputPassword";
+
+function Login() {
+  const [data, setData] = useState({ email: "", password: "" });
+  const [disable, setDisable] = useState(true);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const validEmail = /\S+@\S+\.\S+/;
+  const PASSLEN = 6;
+
+  const validation = () => {
+    const { email, password } = data;
+    if (validEmail.test(email) && password.length >= PASSLEN) {
+      return setDisable(false);
+    }
+    return setDisable(true);
+  };
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  useEffect(() => {
+    validation();
+  }, [data]);
+  const handleJoin = () => {
+    const { email } = data;
+    dispatch(actions.login(email));
+    return history.push("/carteira");
+  };
+  const { email, password } = data;
+
+  return (
+    <main>
+      <InputEmail value={email} onChange={handleChange} />
+      <InputPassword value={password} onChange={handleChange} />
+      <button
+        type="button"
+        name="login"
+        disabled={disable}
+        onClick={handleJoin}
+      >
+        Entrar
+      </button>
+    </main>
+  );
+}
+
+export default Login;
--- src/tests/testConfig.js
+++ src/tests/testConfig.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { applyMiddleware, createStore } from 'redux';
-import { render } from '@testing-library/react';
+import React from "react";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { applyMiddleware, createStore } from "redux";
+import { render } from "@testing-library/react";
 
-import reducer from '../reducers';
+import reducer from "../reducers";
 
 export const getStore = (initialState) => {
   if (!initialState) return createStore(reducer, applyMiddleware(thunk));
   return createStore(reducer, initialState, applyMiddleware(thunk));
 };
 
-export const renderWithRouterAndStore = (component, routeConfigs = {}, initialState) => {
-  const route = routeConfigs.route || '/';
+export const renderWithRouterAndStore = (
+  component,
+  routeConfigs = {},
+  initialState
+) => {
+  const route = routeConfigs.route || "/";
   const store = getStore(initialState);
-  const history = routeConfigs.history
-    || createMemoryHistory({ initialEntries: [route] });
+  const history =
+    routeConfigs.history || createMemoryHistory({ initialEntries: [route] });
 
   return {
     ...render(
-      <Provider store={ store }>
-        <Router history={ history }>{component}</Router>
-      </Provider>,
+      <Provider store={store}>
+        <Router history={history}>{component}</Router>
+      </Provider>
     ),
     history,
     store,
