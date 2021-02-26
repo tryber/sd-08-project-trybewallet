f8006b7abb2f5ebe7b6159f2a5c2994741e00909
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-02-26 03:38:27 -0300

    Iniciando o Requisito 4

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,28 @@
 export const saveEmail = (email) => ({ type: 'SAVE_EMAIL', email });
 
-export const test = (text) => ({ type: 'TEST', text });
+async function fetchCurrencyExchange() {
+  const API = await fetch('https://economia.awesomeapi.com.br/json/all');
+  const currencyExchange = await API.json();
+  return currencyExchange;
+}
+
+export const requestCurrencyExchange = (currency) => (
+  { type: 'REQUEST_CURRENCY', currency });
+
+export const failedRequest = (error) => ({ type: 'FAILE_REQUEST', error });
+
+export const saveCurrencyExchange = () => async (dispatch) => {
+  const currencies = await fetchCurrencyExchange();
+  dispatch(requestCurrencyExchange(currencies));
+};
+
+export const addExpense = (expense) => async (dispatch) => {
+  const currencies = await fetchCurrencyExchange();
+  dispatch({
+    type: 'ADD_EXPENSE',
+    expense: {
+      ...expense,
+      exchangeRate: currencies,
+    },
+  });
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Header({ email }) {
+export default function Header({ email, total }) {
   return (
     <div className="header">
       <h1>TRYBE WALLET</h1>
       <div className="infos-header">
         <p data-testid="total-field">
-          0
+          {total}
           <span data-testid="header-currency-field">BRL</span>
         </p>
         <p data-testid="email-field">{email}</p>
@@ -18,4 +18,5 @@ export default function Header({ email }) {
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
 };
