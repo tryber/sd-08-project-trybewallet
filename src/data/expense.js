653ea3e160b31c679df48581152eee7ec9c22ef9
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-25 23:56:27 -0300

    feat:  create file data expense

---
+++ src/data/expense.js
@@ -0,0 +1,22 @@
+const expense = {
+  cambio: [
+    'USD',
+    'CAD',
+    'EUR',
+    'GBP',
+    'ARS',
+    'BTC',
+    'LTC',
+    'JPY',
+    'CHF',
+    'AUD',
+    'CNY',
+    'ILS',
+    'ETH',
+    'XRP',
+  ],
+  method: ['Dinheiro', 'Cartão de crédito', 'Cartão de débito'],
+  tag: ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'],
+};
+
+export default expense;
