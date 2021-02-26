7f604aa14aa5a1ea04c794eb9e59c9d45887fc71
vivianeflowt <vivianeflowt@gmail.com>
2021-02-26 05:14:56 -0300

    update

--- src/components/FormExpense.jsx
+++ src/components/FormExpense.jsx
@@ -4,20 +4,50 @@ import CurrencySelect from './CurrencySelect';
 import PayMethodSelect from './PayMethodSelect';
 import TagSelect from './TagSelect';
 
+const INITIAL_VALUE = {
+  value: 0,
+  description: '',
+  currency: '',
+  tag: 'Alimentação',
+  method: 'Dinheiro',
+};
+
 function FormExpense() {
+  const [data, setData] = useState(INITIAL_VALUE);
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  const { value, description, currency, tag, method } = data;
   return (
     <form>
       <label htmlFor="value">
         Valor:
-        <input type="text" name="value" data-testid="value-input" />
+        <input
+          type="text"
+          value={ value }
+          name="value"
+          data-testid="value-input"
+          onChange={ handleChange }
+        />
       </label>
       <label htmlFor="description">
         Descrição:
-        <input type="text" name="description" data-testid="value-input" />
+        <input
+          type="text"
+          value={ description }
+          name="description"
+          data-testid="value-input"
+          onChange={ handleChange }
+        />
       </label>
-      <CurrencySelect />
-      <PayMethodSelect />
-      <TagSelect />
+      <CurrencySelect value={ currency } onChange={ handleChange } />
+      <PayMethodSelect value={ method } onChange={ handleChange } />
+      <TagSelect value={ tag } onChange={ handleChange } />
       <button type="button">Adicionar despesa</button>
     </form>
   );
