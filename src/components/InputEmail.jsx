55c23824238a4778397de86853b7ca50fc7e5b6f
vivianeflowt <vivianeflowt@gmail.com>
2021-02-26 03:05:34 -0300

    ok

--- src/components/InputEmail.jsx
+++ src/components/InputEmail.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 export default function InputEmail({ ...props }) {
   return (
     <>
       <label htmlFor="email">
         Email:
-        <input type="text" data-testid="email-input" name="email" {...props} />
+        <input type="text" data-testid="email-input" name="email" { ...props } />
       </label>
       <br />
     </>
