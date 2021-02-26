ec2a3b321423e2c1d437e0a6df3703ee825216ee
wesleypribeiro <wesley_paulo00@hotmail.com>
2021-02-26 18:19:52 -0300

    R02 - Concluido

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import Wallet from './pages/Wallet';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ App } />
-        <Route path="/carteira" component={ Wallet } />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
 
