320964687f93fe138bb9274560ae03c0b0d6e64e
vivianeflowt <vivianeflowt@gmail.com>
2021-02-26 02:58:56 -0300

    update

--- src/components/InputPassword.jsx
+++ src/components/InputPassword.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 export default function InputPassword({ ...props }) {
   return (
-    <label htmlFor="password">
-      Senha:
-      <input
-        type="password"
-        data-testid="password-input"
-        name="password"
-        { ...props }
-      />
-    </label>
+    <>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          { ...props }
+        />
+      </label>
+      <br />
+    </>
   );
 }
