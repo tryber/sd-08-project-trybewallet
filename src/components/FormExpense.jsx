574bcc52c3c3c81c74cb0f451b408964062f92fc
MarceloBorges <melombm@hotmail.com>
2021-02-25 22:14:07 -0500

    adcinando input de despesa

--- src/components/FormExpense.jsx
+++ src/components/FormExpense.jsx
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class FormExpense extends Component {
   render() {
     return (
-      <div />
+      <div>
+        <form>
+          <label htmlFor="value">
+            Despensa:
+            <input
+              type="text"
+              id="value"
+              data-testid="value-input"
+              value={ null }
+              name="value"
+              onChange={ null }
+            />
+          </label>
+
+        </form>
+      </div>
     );
   }
 }
