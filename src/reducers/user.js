d57807d2788f70537b68065571880c0ec078a0f2
Bruno Wedley <brunowedley27@gmail.com>
2021-02-26 16:34:39 -0300

    adiciona requisitos 1, 2 e 3

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+import { EMAIL } from '../actions';
+
+const initialStateUser = {
+  email: '',
+};
+
+export default function user(state = initialStateUser, action) {
+  switch (action.type) {
+  case EMAIL:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+}
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { WALLET } from '../actions';
+
+const initialStateWallet = {
+  currencies: [],
+  expenses: [],
+};
+
+export default function wallet(state = initialStateWallet, action) {
+  switch (action.type) {
+  case WALLET:
+    return { ...state, currencies: action.payload };
+  default:
+    return state;
+  }
+}
