9fc5ca0bdb04241a9e040b6a667a45abaa819e28
Ediberto Oliveira <edibertooliveira@aol.com>
2021-02-26 20:10:04 -0300

    feat: added reducer wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,13 +1,22 @@
-// import { LOGIN } from '../actions';
+import { ADD_EXPENSE, GET_CURRENCIES } from '../actions';
 
 const initialState = {
+  count: 0,
   currencies: [],
   expenses: [],
+  coins: {
+    cambio: 'BRL',
+    total: 0,
+  },
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
-  case 'register':
+  case ADD_EXPENSE:
+    return {
+      ...state, expenses: [...state.expenses, { id: state.count + 1, ...action.payload }],
+    };
+  case GET_CURRENCIES:
     return {
 
     };
