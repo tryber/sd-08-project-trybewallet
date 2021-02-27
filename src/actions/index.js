a64e5b0392eee9f742f6895b7a04bbabb5d4394c
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-02-27 12:59:13 -0300

    Actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,28 @@
 // Coloque aqui suas actions
+export const addEmail = (value) => ({
+  type: 'ADD_ELEMENT',
+  value,
+});
+
+const requestCurrency = () => ({
+  type: 'REQUEST_CURRENCY',
+});
+
+const receiveCurrency = (currency) => ({
+  type: 'RECEIVE_CURRENCY',
+  currency,
+});
+
+export const addExpense = (array) => ({
+  type: 'ADD_EXPENSE',
+  expenses: array,
+});
+
+export function fetchCurrency() {
+  return (dispatch) => {
+    dispatch(requestCurrency());
+    return fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((currency) => dispatch(receiveCurrency(currency)));
+  };
+}
