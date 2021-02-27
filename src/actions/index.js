067a02e6937a18b83fb212e82ae480b73e82704c
Blucilha <blucilha.nascimento@gmail.com>
2021-02-27 10:32:30 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+const USER_LOGIN = 'USER_LOGIN';
+export default function userLogin(email) {
+  return (
+    { type: USER_LOGIN,
+      payload: email,
+    }
+  );
+}
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,12 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {};
+const USER_LOGIN = 'USER_LOGIN';
+function userReducerLogin(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case USER_LOGIN:
+    return { ...state, email: action.payload };
+  default:
+    return state;
+  }
+}
+
+export default userReducerLogin;
