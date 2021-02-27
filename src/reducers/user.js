c647fc8ad2e6066a447fc94fb51c89c714fba799
Edson Araujo de Barros Junior <edson@pop-os.localdomain>
2021-02-27 13:04:42 -0300

    Reducers

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,15 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+function user(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'ADD_ELEMENT':
+    return { ...state, email: action.value };
+  default:
+    return state;
+  }
+}
+
+export default user;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,21 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  isFetching: false,
+  currency: {},
+  error: '',
+  expenses: [],
+};
+
+function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'REQUEST_CURRENCY':
+    return { ...state, isFetching: true };
+  case 'RECEIVE_CURRENCY':
+    return { ...state, currency: action.currency, isFetching: false };
+  case 'ADD_EXPENSE':
+    return { ...state, expenses: [...state.expenses, action.expenses] };
+  default:
+    return state;
+  }
+}
+
+export default wallet;
