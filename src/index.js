2749e5e72a7a2c3c1a189f35a15e5c1646a683f5
Blucilha <blucilha.nascimento@gmail.com>
2021-02-27 11:04:49 -0300

    Requisito 1 parte 2

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/index';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render((
+ReactDOM.render(
   <Router>
     <Provider store={ store }>
       <App />
     </Provider>
-  </Router>
-),
-document.getElementById('root'));
+  </Router>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
