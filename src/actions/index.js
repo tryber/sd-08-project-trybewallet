a25d7660f782681d5a604b35a13541f6854b102e
Thiago Ferreira <thiagocristhian@gmail.com>
2021-02-27 00:27:27 -0300

    Requisito 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+// export const LOGIN = 'LOGIN';
+export const EMAIL = 'EMAIL';
+export const PASS = 'PASS';
+
+export const emailAction = (value) => ({ type: EMAIL, value });
+export const passwordAction = (value) => ({ type: PASS, value });
+
+// export const loginAction = (value) => ({ type: LOGIN, value });
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,22 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { EMAIL, PASS } from '../actions';
+
+const initialState = {
+  email: '',
+  password: '',
+};
+
+function user(state = initialState, action) {
+  switch (action.type) {
+  // case USER:
+  //   return { ...state, email: action.value.email, password: action.value.password };
+  case EMAIL:
+    return { ...state, email: action.value };
+  case PASS:
+    return { ...state, password: action.value };
+  default:
+    return state;
+  }
+}
+
+export default user;
