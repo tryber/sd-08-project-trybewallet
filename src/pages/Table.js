3e1bd69a289b45318efa7dfaf28ddc5b12c93daf
Clênio Galvão <clenio.cgm@gmail.com>
2021-02-27 20:08:58 -0300

    resolve questao 5

---
+++ src/pages/Table.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Table extends React.Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+        { expenses.map((expense) => (
+          <tr key={ expense.id }>
+            <td>{ expense.description }</td>
+            <td>{ expense.tag }</td>
+            <td>{ expense.method }</td>
+            <td>{ parseFloat(expense.value).toFixed(2) }</td>
+            <td>{ expense.currency }</td>
+            <td>
+              {
+                parseFloat(expense.exchangeRates[expense.currency].ask)
+                  .toFixed(2)
+              }
+            </td>
+            <td>
+              {
+                parseFloat((expense.value)
+                * (expense.exchangeRates[expense.currency].ask))
+                  .toFixed(2)
+              }
+            </td>
+            <td>Real</td>
+            <td>
+              <button type="button">editar</button>
+              <button type="button" data-testid="delete-btn">x</button>
+            </td>
+          </tr>
+        )) }
+      </table>
+    );
+  }
+}
+
+Table.propTypes = {
+  expenses: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
