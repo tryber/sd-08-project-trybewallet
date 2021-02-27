ae2fd03be7f70cb351efed2e248da43577b6c037
Antonio Schappo <antonioschappo@gmail.com>
2021-02-27 16:51:16 -0300

    First req lint correction

--- src/App.js
+++ src/App.js
@@ -13,17 +13,3 @@ function App() {
 }
 
 export default App;
-
-// Outra coisa importante: devido a estrutura que o avaliador utiliza para realizar os testes, 
-// é **necessário** que o seu `<Provider />` e o seu `<BrowserRouter />` estejam no arquivo `index.js` 
-// e **não** no `<App />`.
-// estado global:
-// {
-//   user: {
-//     email: '',
-//   },
-//   wallet: {
-//     currencies: [],
-//     expenses: []
-//   }
-// }
