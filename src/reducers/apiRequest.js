1bd7700142fb99216ecc06fcf05f01142c2bea97
hugobrr <hugobrr@gmail.com>
2021-02-28 19:26:58 -0300

    vendo quantos requisitos eu já completei

---
+++ src/reducers/apiRequest.js
@@ -0,0 +1,31 @@
+import { REQUEST_API, REQUEST_API_SUCCESS, REQUEST_API_ERRO } from '../store/consts';
+
+const initialState = {
+  exchangeRates: '',
+};
+
+function apiRequest(state = initialState, { type, payload }) {
+  switch (type) {
+  case REQUEST_API:
+    return {
+      ...state,
+      isFetching: payload.isFetching,
+    };
+  case REQUEST_API_SUCCESS:
+    return {
+      ...state,
+      exchangeRates: payload,
+      isFetching: payload.isFetching,
+    };
+  case REQUEST_API_ERRO:
+    return {
+      ...state,
+      error: payload.error,
+      isFetching: payload.isFetching,
+    };
+  default:
+    return state;
+  }
+}
+
+export default apiRequest;
