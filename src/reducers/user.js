e7fd444c1fb1cbd6c183157ab8cec55295e6ae9f
Rosiele David <rosieleserpa@hotmail.com>
2021-02-28 15:37:37 -0300

    requisito 1: cria a pagina inicial de login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,20 @@
+import { SAVE_EMAIL } from '../actions';
+
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  email: '',
+};
+
+const saveUser = (state = INITIAL_STATE, { type, email }) => {
+  switch (type) {
+  case SAVE_EMAIL:
+    return {
+      ...state,
+      email,
+    };
+  default:
+    return state;
+  }
+};
+
+export default saveUser;
