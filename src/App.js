94bc52320672290df4a556ce6516579f69b61b86
Cleberton Francisco <clebertonfgc@gmail.com>
2021-02-28 18:14:03 -0300

    Requisito 01 'login'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/carteira" component={ wallet } />
+    </Switch>
+
+  );
 }
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const LOGIN_USER_EMAIL = 'LOGIN_USER_EMAIL';
+
+export const addEmailStore = (email) => ({
+  type: LOGIN_USER_EMAIL,
+  email,
+});
