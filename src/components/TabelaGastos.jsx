b82786252a0f4b1f4f73af64bb4a91f4144063fa
Bruno Martins <bruno.martins217@gmail.com>
2021-02-28 01:39:43 -0300

    :package: Requisito 5

--- src/components/TabelaGastos.jsx
+++ src/components/TabelaGastos.jsx
@@ -1,54 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Table from 'react-bootstrap/Table';
 
 class TabelaGastos extends Component {
   render() {
     const { expenses } = this.props;
     return (
       <div>
-        <Table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar</th>
-              <th>Excluir</th>
-            </tr>
-          </thead>
-          <tbody>
-            {expenses.map((
-              { id, value, description, currency, method, tag, exchangeRates },
-            ) => (
-              <tr key={ id }>
-                <td>{id}</td>
-                <td>{description}</td>
-                <td>{tag}</td>
-                <td>{method}</td>
-                <td>{value}</td>
-                <td>{exchangeRates[currency].name}</td>
-                <td>
-                  {((
-                    Math.round(exchangeRates[currency].ask * 100) / 100)).toFixed(2)}
-                </td>
-                <td>
-                  {((
-                    Math.round(value * exchangeRates[currency].ask)
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((
+            { id, value, description, currency, method, tag, exchangeRates },
+          ) => (
+            <tr key={ id }>
+              <td>{id}</td>
+              <td>{description}</td>
+              <td>{tag}</td>
+              <td>{method}</td>
+              <td>{value}</td>
+              <td>{exchangeRates[currency].name}</td>
+              <td>
+                {((
+                  Math.round(exchangeRates[currency].ask * 100) / 100)).toFixed(2)}
+              </td>
+              <td>
+                {((
+                  Math.round(value * exchangeRates[currency].ask)
                     * 100) / 100).toFixed(2)}
-                </td>
-                <td>Real</td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+              </td>
+              <td>Real</td>
+            </tr>
+          ))}
+        </tbody>
       </div>
     );
   }
