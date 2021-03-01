d5ed582822e04a9936453332563fb5dcf714c19e
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-03-01 18:17:18 -0300

    Implementado e criado arquivos para atender o requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+
+import './App.css';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div className="App">
+      <h1>Trybe Wallet</h1>
+      <Switch>
+        <Route path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
