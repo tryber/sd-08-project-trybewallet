cdd852b14d8561c754da4273db667731498a0c81
amandaos <amanda.775.souza@gmail.com>
2021-03-01 15:47:54 -0300

    feature: cria header para wallet

---
+++ src/components/HeaderWallet.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class HeaderWallet extends Component {
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <p data-testid="email-field">
+          {email}
+        </p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
+    );
+  }
+}
+
+HeaderWallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = ({ user: { email } }) => ({
+  email,
+});
+
+export default connect(mapStateToProps)(HeaderWallet);
