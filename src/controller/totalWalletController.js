3cca6357d4b8aeb746ab0cffc43bbc4e35c330ae
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-01 12:10:13 -0300

    feat: conversor exchange

--- src/controller/totalWalletController.js
+++ src/controller/totalWalletController.js
@@ -16,3 +16,19 @@ export function totalMoneyInTheWallet(expensesData, placeOfAcronym) {
 
   return formatMoney(combineExpenseValue, placeOfAcronym);
 }
+
+export function convertedValueExchange(exchangeRates, currency, value) {
+  const exchangeRate = [exchangeRates].map((
+    exchangeKey,
+  ) => ({
+    name: exchangeKey[currency].name,
+    ask: exchangeKey[currency].ask }))[0];
+
+  const { name, ask } = exchangeRate;
+  const convertedValue = (value * Number(ask)).toFixed(2);
+  return {
+    name,
+    ask,
+    convertedValue,
+  };
+}
