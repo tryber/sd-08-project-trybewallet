e3f275182e3a533616e988dd9766188b083d6199
wagnerberna <cabelo29007@gmail.com>
2021-03-01 05:55:24 -0300

    req01 rota

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <div>
+      <h1>Hello, TrybeWallet!</h1>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
