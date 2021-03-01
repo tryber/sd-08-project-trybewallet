6c489fd6c266b061755a4604814b595371f6436d
wagnerberna <cabelo29007@gmail.com>
2021-03-01 07:14:33 -0300

    req01 form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email:'',
+      password:'',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h2>Login</h2>
+        <label htmlFor="email">
+          E-mail:
+          <input
+            type="text"
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="password">
+          E-mail:
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+        </label>
+      </form>
+    );
   }
 }
 
