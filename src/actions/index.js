0bb4da35fb60c4b4f4a39919470ad0506b31201a
Demistocles <demi.treze@gmail.com>
2021-03-01 19:03:12 -0300

    primeiro requisito

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-// Coloque aqui suas actions
+export const INPUT_HANDLE = 'INPUT_HANDLE';
+export const LOGIN_USER = 'LOGIN_USER';
+
+export const inputHandle = (position, input) => ({
+  type: INPUT_HANDLE,
+  [position]: input,
+});
+
+export const handleLogin = (userEmail) => ({
+  type: LOGIN_USER,
+  user: { email: userEmail },
+});
