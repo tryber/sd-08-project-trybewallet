61a3dd7884ceb5fbf3f5c5bc45d7051b09595900
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-03-01 18:39:59 -0300

    Implementado rota para atender requisito 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <h1>Trybe Wallet</h1>
       <Switch>
+        <Route path="/carteira" component={ Wallet } />
         <Route path="/" component={ Login } />
       </Switch>
     </div>
