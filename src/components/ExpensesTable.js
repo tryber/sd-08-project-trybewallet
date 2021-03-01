40cf92d9d1bb3735fd9314e3de5606c98d01020c
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-02-28 21:29:21 -0300

    cria o cabeçalho da tabela de despesas

---
+++ src/components/ExpensesTable.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './ExpensesTable.css';
+
+class ExpensesTable extends Component {
+  render() {
+    return (
+      <table>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default ExpensesTable;
