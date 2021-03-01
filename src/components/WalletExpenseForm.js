d589f87ca4da25271ec06a004f4698769667a6ce
Ediberto Oliveira <edibertooliveira@aol.com>
2021-03-01 07:29:00 -0300

    feat: added components form expense

---
+++ src/components/WalletExpenseForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class WalletExpenseForm extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+// WalletExpenseForm.propTypes = {
+
+// };
+
+export default WalletExpenseForm;
---
+++ src/components/WalletSpent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class WalletSpent extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+// WalletSpent.propTypes = {
+
+// };
+
+export default WalletSpent;
