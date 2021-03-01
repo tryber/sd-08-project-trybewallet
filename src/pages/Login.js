677c53dd6731805866541623bb52328160b36830
amandaos <amanda.775.souza@gmail.com>
2021-03-01 15:02:16 -0300

    feature: cria pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { saveEmail } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  validateLogin() {
+    const { email, password } = this.state;
+    const isEmail = /\S+@\S+\.\S+/;
+    const minimumPasswordSize = 6;
+    return !isEmail.test(email) || (password.length < minimumPasswordSize);
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <form>
+          E-mail:
+          <input
+            type="text"
+            value={ email }
+            name="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+          />
+          Senha:
+          <input
+            type="password"
+            value={ password }
+            name="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+          />
+          <Link
+            to="/carteira"
+          >
+            <button
+              type="button"
+              disabled={ this.validateLogin() }
+              onClick={ () => saveEmail(email) }
+            >
+              Entrar
+            </button>
+          </Link>
+        </form>
+      </div>
+    );
   }
 }
 
