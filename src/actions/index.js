a20c56c8d2602146ed06652f3a11e21d16a5828e
Uilton Rocha <itoclick@gmail.com>
2021-03-01 13:49:47 -0300

    tela login finalizada

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,28 +1,53 @@
 // Coloque aqui suas actions
 
-export const addUseract = () => {
-  const inpEml = document.getElementById('inpEml');
+const USER_LOGIN = 'USER_LOGIN';
+const WALLET_ADDCURR = 'WALLET_ADDCURR';
+const WALLET_ADDEXP = 'WALLET_ADDEXP';
+const WALLET_DELEXP = 'WALLET_DELEXP';
+const WALLET_EDITEXP = 'WALLET_EDITEXP';
+const WALLET_ENTER_EDIT_MODE = 'WALLET_ENTER_EDIT_MODE';
+const WALLET_EXIT_EDIT_MODE = 'WALLET_EXIT_EDIT_MODE';
+
+export function login(payload) {
   return {
-    type: 'USR_ADD',
-    email: inpEml.value,
+    type: USER_LOGIN,
+    payload,
   };
-};
+}
 
-export const inwalletact = () => ({
-  type: 'MOEDA',
-  ...state,
-  wallet: {
-    currencies: [action.currencies],
-  },
-});
+export function addCurrency(payload) {
+  return {
+    type: WALLET_ADDCURR,
+    payload,
+  };
+}
+export function addExpense(payload) {
+  return {
+    type: WALLET_ADDEXP,
+    payload,
+  };
+}
+export function delExpense(id) {
+  return {
+    type: WALLET_DELEXP,
+    payload: id,
+  };
+}
+export function editExpense(payload) {
+  return {
+    type: WALLET_EDITEXP,
+    payload,
+  };
+}
 
-export const outwalletact = () => ({
-  type: 'MONEI_OUT',
-  ...state,
-  wallet: { expenses: [action.expenses] },
-
-});
-
-/* const xablau = 'xablau';
-console.log(xablau);
- */
+export function enterEditMode(id) {
+  return {
+    type: WALLET_ENTER_EDIT_MODE,
+    id,
+  };
+}
+export function exitEditMode() {
+  return {
+    type: WALLET_EXIT_EDIT_MODE,
+  };
+}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,39 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import * as actions from '../actions';
 import './login.css';
 
-/* const loginCheck = (target) => {
-  let passou = false;
-  const inputMail = document.getElementsByClassName('ok');
-  const exp = new RegExp(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/);
-  passou = exp.test(inputMail.text);
-  if (passou === false) { target.disabled = true; } else { target.disabled = false; }
-  console.log(passou);
-  return passou;
-}; */
+import Inputmail from '../components/inputmail';
+import InputPass from '../components/inputpass';
 
-export default class Login extends React.Component {
-  render() {
-    return (
-      <div className="campos_login">
-        <label Faça login
-          <input
-            type="email"
-            pattern="/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/"
-            classename="inputeml"
-            data-testid="email-input"
-            title="Formato correto: email@dominio.com.br"
-          />
-        />
-        <input type="password" data-testid="password-input" minLength="3" />
-        <button
-          name="button"
-          type="button"
-          data-testid="btnEntrar"
-        // onClick={ loginCheck() }
-        >
-          Entrar
-        </button>
-      </div>
-    );
-  }
+function Login() {
+  const [data, setData] = useState({ email: '', password: '' });
+  const [disable, setDisable] = useState(true);
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const patternEmail = /\S+@\S+\.\S+/;
+  // const { Pattern } = Inputmail; //não consegui usar o dado dai
+  const PASSLEN = 6;
+  const validation = () => {
+    const { email, password } = data;
+    if (patternEmail.test(email) && password.length >= PASSLEN) {
+      return setDisable(false);
+    }
+    return setDisable(true);
+  };
+  // usando o nome como chave.
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+  useEffect(() => {
+    validation();
+  }, [data]);
+  const handleJoin = () => {
+    const { email } = data;
+    dispatch(actions.login(email));
+    // testar se returna e muda a rota no final
+    return history.push('/carteira');
+  };
+  const { email, password } = data;
+  return (
+    <div className="campos_login">
+      <Inputmail value={ email } onChange={ handleChange } />
+      <InputPass value={ password } onChange={ handleChange } />
+      <button
+        className="login"
+        type="button"
+        name="login"
+        disabled={ disable }
+        onClick={ handleJoin }
+      >
+        Entrar
+      </button>
+    </div>
+  );
 }
+
+export default Login;
