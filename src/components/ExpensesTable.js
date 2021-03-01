d6dc4e1bcc9fddfa2f6d83729be0b31a9feb3eea
Rafael Maltez <rafaelmaltez89@gmail.com>
2021-03-01 15:32:13 -0300

    cria a funçao que exibe as despesas na tabela

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './ExpensesTable.css';
 
 class ExpensesTable extends Component {
+  constructor() {
+    super();
+    this.renderTableHeader = this.renderTableHeader.bind(this);
+    this.renderTableRows = this.renderTableRows.bind(this);
+  }
+
+  renderTableHeader() {
+    return (
+      <tr>
+        <th>Descrição</th>
+        <th>Tag</th>
+        <th>Método de pagamento</th>
+        <th>Valor</th>
+        <th>Moeda</th>
+        <th>Câmbio utilizado</th>
+        <th>Valor convertido</th>
+        <th>Moeda de conversão</th>
+        <th>Editar/Excluir</th>
+      </tr>
+    );
+  }
+
+  renderTableRows() {
+    const { expenses } = this.props;
+    return expenses.map((expense) => {
+      const exchangeRate = expense.exchangeRates[`${expense.currency}`].ask;
+      const coinName = expense.exchangeRates[`${expense.currency}`].name;
+      return (
+        <tr key={ expense.id }>
+          <td>{ expense.description }</td>
+          <td>{ expense.tag }</td>
+          <td>{ expense.method }</td>
+          <td>{ expense.value }</td>
+          <td>{ coinName }</td>
+          <td>{ exchangeRate }</td>
+          <td>{ parseFloat(expense.value) * parseFloat(exchangeRate) }</td>
+          <td>BRL</td>
+          <td><button type="button">Excluir</button></td>
+        </tr>
+      );
+    });
+  }
+
   render() {
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
+        { this.renderTableHeader() }
+        { this.renderTableRows() }
       </table>
     );
   }
 }
 
-export default ExpensesTable;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(ExpensesTable);
