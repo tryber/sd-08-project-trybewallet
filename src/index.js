9632d7c2d25e7637b4150fc514de27411dcc326a
wellington <profwellington17@gmail.com>
2021-03-01 21:20:32 -0300

    requisito1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+
+
+
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+      <BrowserRouter>
+        <Provider store= { store }>
+          <App />
+        </Provider>
+      </BrowserRouter>,
   document.getElementById('root'),
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import getEmail from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      email: '',
+      password: '',
+      button: true,
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.verifyEmail = this.verifyEmail.bind(this);
+  }
+
+  handleChange({target}) {
+    const { name, value }= target;
+    this.setState({ 
+      [name]: value
+    });
+    this.verifyEmail()
+  }
+
+  verifyEmail() {
+    const { email, password }= this.state;
+    const passwordSizemini=5;
+    if (email.match(/^\w{3,}@\w+\.\w{2,6}(\.\w{2})?[^\s+]$/gm)
+    && password.length >= passwordSizemini) {
+      this.setState({
+        button: false,
+      });
+    } else {
+      this.setState({
+        button: true,
+      });
+    }
+  }
+  
+
+
   render() {
-    return <div>Login</div>;
+    const { saveEmail }= this.props;
+    const { email, button }= this.state;
+    return (
+       <div>
+         <form>
+            <input name="email" placeholder="Email" onChange={this.handleChange} value={this.state.email} data-testid="email-input" type="text" /> < p/>
+            <input name ="password" placeholder="Senha" onChange={this.handleChange} value={this.state.password} data-testid="password-input" type="password" /> < p/>
+             <button disabled={ button } onClick={ () => saveEmail(email) } type="button"><Link to="/carteira">Entrar</Link></button>
+         </form>
+        
+      </div>
+    );
+      
   }
 }
 
-export default Login;
+
+Login.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+}
+const mapDispatchToProps = (dispatch) => {
+  return ({
+    saveEmail: (email) => dispatch(
+      getEmail(email)
+    ),
+  });
+}
+
+export default connect(null, mapDispatchToProps)(Login);
