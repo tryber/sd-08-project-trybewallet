383d9ef887e4d1bc65c0ba0580e129c36ec55cb5
victor Farias <victorfarias.tv@gmail.com>
2021-03-02 11:34:45 -0300

    Requisito 3

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+import { REQUEST_START, REQUEST_SUCCESS, REQUEST_FAIL, SAVE_EXPENSE } from '../actions';
+
+const INITIAL_STATE_WALLET = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE_WALLET, action) => {
+  switch (action.type) {
+  case REQUEST_START:
+    return { ...state, isFetching: true };
+  case REQUEST_SUCCESS:
+    return { ...state, isFetching: false, currencies: [action.currencies] };
+  case REQUEST_FAIL:
+    return { ...state, isFetching: false, error: action.error };
+  case SAVE_EXPENSE:
+    return { ...state, expenses: [...state.expenses, action.expenses] };
+  default:
+    return state;
+  }
+};
+
+export default wallet;
