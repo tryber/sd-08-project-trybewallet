488d11e68acd6f48f0e3691f773c3ee73b4ad580
josebejohann <josebernardojohann@gmail.com>
2021-03-01 21:48:21 -0300

    Creates a <Header /> component for the wallet page

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { email } = this.props;
+
+    return (
+      <>
+        <p data-testid="email-field">{email}</p>
+
+        <span data-testid="total-field">0</span>
+        <span data-testid="header-currency-field">BRL</span>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Header);
