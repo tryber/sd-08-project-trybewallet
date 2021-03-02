29678adb070c01426e7b91bf08a071ace65a566b
wagnerberna <cabelo29007@gmail.com>
2021-03-02 08:22:18 -0300

    req04 estrutura

---
+++ src/components/WalletTable.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class WalletTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Tabela</h1>
+
+      </div>
+    );
+  }
+}
+
+export default WalletTable;
