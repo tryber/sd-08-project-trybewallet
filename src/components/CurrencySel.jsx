6e3651189c3b2a28974ed4d2f8cd4b3b553b7eee
Uilton Rocha <itoclick@gmail.com>
2021-03-03 03:03:44 -0300

    tela da carteira e o req-4

--- src/components/CurrencySel.jsx
+++ src/components/CurrencySel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
-import * as fetch from 'node-fetch';
+// import * as fetch from 'node-fetch';
 import * as actions from '../actions';
 
 export async function getCurrencyList() {
