be4011bb8add24454f1d9b025a3e3ee898f6d184
Uilton Rocha <itoclick@gmail.com>
2021-03-04 00:25:20 -0300

    ok

--- src/components/CurrencySel.jsx
+++ src/components/CurrencySel.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { useDispatch, useSelector, connect } from 'react-redux';
 
 import * as actions from '../actions';
 
+const arr = [];
 async function getCurrencyList() {
   return fetch('https://economia.awesomeapi.com.br/json/all')
     .then((res) => res.json())
-    .then((data) => Object.keys(data).filter((i) => i !== 'USDT'));
+    .then((data) => arr.push(Object.keys(data).filter((i) => i !== 'USDT')));
 }
+getCurrencyList();
 
-function CurrencySelect({ onChange, ...rest }) {
-  const currList = useSelector((state) => state.wallet.currencies);
-  const dispatch = useDispatch();
-  // const fetchCurrList = async () => {
-  //  if (currList.length === 0) {
-  // const list = await getCurrencyList();
-  // dispatch(actions.addCurrency(list));
-  //  }
-  // };
-
-  // fetchCurrList();
+function CurrencySelect() {
+  console.log(arr);
   return (
     <select
       name="currency"
       data-testid="currency-input"
       role="combobox"
-      onChange={ onChange }
-      { ...rest }
+      // onChange={ onChange }
+      // { ...rest }
     >
-      {currList && currList.map((curr) => (
+      { arr.map((curr) => (
         <option
           key={ curr }
           data-testid={ curr }
@@ -41,14 +34,14 @@ function CurrencySelect({ onChange, ...rest }) {
 
   );
 }
-
+/*
 CurrencySelect.defaultProps = {
   onChange: null,
 };
 
 CurrencySelect.propTypes = {
   onChange: PropTypes.func,
-};
+}; */
 
 const mapDispatchToProps = (dispatch) => ({
   addCurrency: dispatch(actions.addCurrency),
