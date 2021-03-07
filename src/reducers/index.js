9fdc0dee01b5ef01fdcd558ec54de9257c2db93a
Daniel Leon <daniel.radialista@gmail.com>
2021-03-07 13:34:33 -0300

    requisito 3

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 
 import user from './user';
-// import wallet from './wallet';
+import wallet from './wallet';
 
 export default combineReducers({
   user,
+  wallet,
 });
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,6 +1,10 @@
 import { Types } from '../actions/user';
 
-const user = (state = {}, action) => {
+const INITIAL_STATE = {
+  email: '',
+};
+
+const user = (state = { INITIAL_STATE }, action) => {
   switch (action.type) {
   case Types.SAVE_EMAIL: return { ...state, email: action.payload };
   default: return state;
