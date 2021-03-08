48be445db343501953380a15a8e24753b8b01bd6
Felip Franca <felipefrancafj@gmail.com>
2021-03-08 04:19:00 -0300

    FIX: ESLint

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login }/>
+      <Route exact path="/" component={ Login } />
     </Switch>
   );
-};
+}
