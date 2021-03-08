9abed35f238f26a397ddf2b4c45fded4772db14c
Bardo <martins7ribeiro@gmail.com>
2021-03-08 02:03:13 -0300

    pequenos progressos - lint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,17 @@
-import * as types from './types';
+// import * as types from './types';
 
 // Coloque aqui suas actions
-const currenciesFromAPI = (currency) => ({
-  type: FETCH_CURRENCIES,
-  payload: currency,
-});
+// const currenciesFromAPI = (currency) => ({
+//   type: FETCH_CURRENCIES,
+//   payload: currency,
+// });
 
-const currenciesFetchAction = () => (dispatch) => {
-  const endpoint = ('https://economia.awesomeapi.com.br/json/all');
-  const currencies = fetch(endpoint);
-  currencies.then((response) => response.json)
-    .then((data) => dispatch(currenciesFromAPI(data)));
-};
+// const currenciesFetchAction = () => (dispatch) => {
+//   const endpoint = ('https://economia.awesomeapi.com.br/json/all');
+//   const currencies = fetch(endpoint);
+//   currencies.then((response) => response.json)
+//     .then((data) => dispatch(currenciesFromAPI(data)));
+// };
 
 // const currenciesFromAPI = () => {
 //   return (dispatch) => {
