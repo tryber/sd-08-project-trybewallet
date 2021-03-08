bddd8cea4ad7a1c543dcc30ff9e4b359c3e04d91
Felip Franca <felipefrancafj@gmail.com>
2021-03-08 04:27:36 -0300

    REQ - 4: Implemented

---
+++ src/components/Category.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Category extends Component {
+  render() {
+    const { datatestid, name, onChange, value } = this.props;
+    return (
+      <select
+        value={ value }
+        onChange={ onChange }
+        name={ name }
+        data-testid={ datatestid }
+      >
+        <option>Alimentação</option>
+        <option>Lazer</option>
+        <option>Trabalho</option>
+        <option>Transporte</option>
+        <option>Saúde</option>
+      </select>
+    );
+  }
+}
+Category.propTypes = {
+  datatestid: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Category;
