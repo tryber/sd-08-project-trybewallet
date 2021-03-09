dfb3d7699a4b6670be08a74cbaeb1cbd7443e138
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-03-08 22:31:49 -0300

    :rocket: Requisito 2 e 3, implementando component Header

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 // Coloque aqui suas actions
 export const SAVE_EMAIL = 'SAVE_EMAIL';
 export const SAVE_PASSWORD = 'SAVE_PASSWORD';
+export const TOTAL_EXPENSE = 'TOTAL_EXPENSE';
 
 export const saveEmail = (email) => ({
   type: SAVE_EMAIL,
@@ -10,4 +11,9 @@ export const saveEmail = (email) => ({
 export const savePassword = (password) => ({
   type: SAVE_PASSWORD,
   password,
+});
+
+export const totalExpense = (totalValue) => ({
+  type: TOTAL_EXPENSE,
+  totalValue,
 });
