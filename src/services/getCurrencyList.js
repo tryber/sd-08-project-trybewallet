ad9770b5c9b3aaf6c47ebafb9e8bab8031154544
rodolfo_oliveira <rod.vgo@gmail.com>
2021-03-08 22:25:20 -0300

    req-4 erros

--- src/services/getCurrencyList.js
+++ src/services/getCurrencyList.js
@@ -1,7 +1,8 @@
 export default async function getCurrencyList() {
   return fetch('https://economia.awesomeapi.com.br/json/all')
     .then((response) => response.json())
-    .then((currencies) => Object.keys(currencies).filter((currency) => i !== 'USDT'));
+    .then((currencies) => Object.keys(currencies)
+      .filter((currency) => currency !== 'USDT'));
 }
 
 export async function fetchCurrency() {
