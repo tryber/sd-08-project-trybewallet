1aafae3cd116cd05db3cab97b2fd5d68c3702f6e
Arthur Nunes <coletivosuburbano@gmail.com>
2021-03-11 02:28:33 -0300

    estruturação-básica

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
 
-class Login extends React.Component {
-  render() {
-    return <div>Login</div>;
-  }
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const minPwdLength = 6;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoggedIn(true);
+  };
+  if (isLoggedIn) return <Redirect to="/carteira" />;
+  return (
+    <form onSubmit={ handleSubmit }>
+      <label htmlFor="email">
+        Email
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+      </label>
+      <label htmlFor="password">
+        Senha
+        <input
+          type="password"
+          name="password"
+          id="password "
+          placeholder="Digite a sua senha"
+          data-testid="password-input"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+      </label>
+      <button
+        type="submit"
+        disabled={
+          !email.match(
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+          ) && !password.length >= minPwdLength
+        }
+      >
+        Entrar
+      </button>
+    </form>
+  );
 }
 
 export default Login;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,52 @@
 import React from 'react';
 
-class Wallet extends React.Component {
-  render() {
-    return <div>TrybeWallet</div>;
-  }
+function Wallet() {
+  return (
+    <>
+      <header>
+        <p data-testid="email-field">email</p>
+        <p data-testid="total-field">despesa total: R$ 0</p>
+        <p data-testid="header-currency-field">Moeda: BRL</p>
+      </header>
+      <main>
+        <form>
+          <label htmlFor="value-input">
+            Valor da despesa:
+            <input type="number" name="value-input" id="value-input" data-testid="value-input" />
+          </label>
+          <label htmlFor="description-input">
+            Descrição da despesa:
+            <input type="text" data-testid="description-input" name="description-input" id="description-input" />
+          </label>
+          <label htmlFor="currency-input">
+            Moeda da despesa:
+            <select name="currency-input" id="currency-input" data-testid="currency-input">
+              <option value="USD" data-testid="USD">USD</option>
+              <option value="CAD" data-testid="CAD">CAD</option>
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            Método de pagamento:
+            <select name="method-input" id="method-input" data-testid="method-input">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartao-credito">Cartão de crédito</option>
+              <option value="cartao-debito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag-input">
+            Categoria da despesa:
+            <select name="tag-input" id="tag-input" data-testid="tag-input">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+          <button type="submit">Adicionar despesa</button>
+        </form>
+      </main>
+    </>);
 }
 
 export default Wallet;
