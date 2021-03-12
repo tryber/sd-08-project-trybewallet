0d05614f39e08cf1d53480eb38ec0949312698ee
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:22:47 -0300

    insertions fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,89 +1,67 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { saveEmail, savePassword } from '../actions';
+import PropTypes from 'prop-types';
 
-function validEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
-}
+import Header from '../components/Header';
+import ExpenseForm from '../components/ExpenseForm';
+import CurrentExpenses from '../components/CurrentExpenses';
 
-const passwordLength = 6;
+import { fetchCurrencies as fetchCurrenciesAction } from '../actions';
 
-function validPassword(password) {
-  return password.length >= passwordLength;
-}
-
-class Login extends React.Component {
-  inputValidation() {
-    const { email, password } = this.props;
-    const emailValidation = validEmail(email);
-    const passwordValidation = validPassword(password);
-
-    if (emailValidation && passwordValidation) {
-      return false;
-    }
-    return true;
-  }
-
-  handleChange({ target: { name, value } }) {
-    this.setState({ [name]: value });
+class Wallet extends React.Component {
+  componentDidMount() {
+    const { fetchCurrencies } = this.props;
+    fetchCurrencies();
   }
 
   render() {
-    const { email, password, dispatchAddEmail, dispatchAddPassword } = this.props;
+    const { userEmail, currencies, isLoading } = this.props;
+
+    // if (!userEmail) return <Redirect to="/" />;
+
+    const availableCurrencies = currencies
+      ? currencies.map((currency) => currency.currency) : [];
 
     return (
-      <div>
-        <h1>MY WALLET</h1>
-        <label htmlFor="email-input">
-          Email
-          <input
-            data-testid="email-input"
-            name="email"
-            onChange={ (e) => dispatchAddEmail(e.target.value) }
-            placeholder="Write your email"
-            value={ email }
-            type="email"
-          />
-        </label>
-        <label htmlFor="password-input">
-          Password
-          <input
-            data-testid="password-input"
-            name="password"
-            onChange={ (e) => dispatchAddPassword(e.target.value) }
-            placeholder="password"
-            value={ password }
-            type="password"
-          />
-        </label>
-        <Link to="/carteira">
-          <button
-            disabled={ this.inputValidation() }
-            type="button"
-            onClick={ () => dispatchAddEmail(email) }
-          >
-            Entrar
-          </button>
-        </Link>
-      </div>
+      <>
+        <Header user={ userEmail } />
+        <main>
+          {
+            isLoading
+              ? <h1>Loading...</h1>
+              : (
+                <ExpenseForm
+                  availableCurrencies={ availableCurrencies }
+                />
+              )
+          }
+          <CurrentExpenses />
+        </main>
+      </>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.user.email,
-  password: state.user.password,
+  userEmail: state.user.email,
+  expenses: state.wallet.expenses,
+  currencies: state.wallet.currencies,
+  isLoading: state.wallet.isLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  dispatchAddEmail: (email) => dispatch(saveEmail(email)),
-  dispatchAddPassword: (password) => dispatch(savePassword(password)),
+  fetchCurrencies: () => dispatch(fetchCurrenciesAction()),
 });
 
-Login.propTypes = {
-  dispatch: PropTypes.func,
-}.isRequired;
+Wallet.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+  currencies: PropTypes.arrayOf(
+    PropTypes.shape({
+      currency: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  fetchCurrencies: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
