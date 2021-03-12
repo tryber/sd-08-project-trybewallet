ec5a949bfc2a5dcb87c9f8b28ecaf9353b0df98d
Caio Menezes Rocha <caiomzrocha@gmail.com>
2021-03-12 14:56:54 -0300

    [REQ-03][ADD] header - init

---
+++ src/components/WalletHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import store from '../store';
+
+class WalletHeader extends React.Component {
+  render() {
+    return <h1>HEADER</h1>;
+  }
+}
+
+// const mapStateToProps = () => {};
+
+export default WalletHeader;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import WalletHeader from '../components/WalletHeader';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return <WalletHeader />;
   }
 }
 
