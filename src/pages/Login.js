678f5f4af8f4ead8aee85258dd35755c38fb2bb2
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 08:35:36 -0300

    fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { saveEmail, savePassword } from '../actions';
+import { saveEmail, savePassword } from '../actions/index';
 
 function validEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
