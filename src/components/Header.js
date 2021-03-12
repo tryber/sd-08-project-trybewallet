67285518416a84e1e5c7f6e4c73130ada157f8e4
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:50:59 -0300

    fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,49 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.sumOftotalExpenses = this.sumOftotalExpenses.bind(this);
-  }
-
-  sumOftotalExpenses() {
-    const { expenses } = this.props;
-    const total = expenses.reduce((acc, current) => (
-      acc + current.value * current.exchangeRates[current.currency].ask
-    ), 0);
-
-    return total.toFixed(2);
-  }
-
   render() {
-    const { email } = this.props;
-    return (
-      <div>
-        <p>
-          <strong>Email:</strong>
-          <span data-testid="email-field">{ email }</span>
-        </p>
-        <p>
-          <strong>Despesa Total:</strong>
-          <span data-testid="total-field">{ this.sumOftotalExpenses() }</span>
-          <strong>Câmbio:</strong>
-          <span data-testid="header-currency-field">BRL</span>
-        </p>
+    const { user, expenses } = this.props;
 
-      </div>
+    const total = expenses.reduce(
+      (acc, expense) => (
+        acc + (parseInt(expense.value, 10)
+        * expense.exchangeRates[expense.currency].ask)
+      ), 0,
+    );
+
+    return (
+      <header>
+        <p>
+          Usuário:
+          {' '}
+          <span data-testid="email-field">
+            {user}
+          </span>
+        </p>
+        <p>
+          Despesas: R$
+          <span data-testid="total-field">{total.toFixed(2)}</span>
+        </p>
+        <p>
+          Moeda de Câmbio:
+          {' '}
+          <span data-testid="header-currency-field">
+            BRL
+          </span>
+        </p>
+      </header>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  email: state.user.email,
   expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
-  email: PropTypes.string.isRequired,
-  expenses: PropTypes.arrayOf(PropTypes.number).isRequired,
+  user: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+
+  })).isRequired,
 };
 
 export default connect(mapStateToProps)(Header);
