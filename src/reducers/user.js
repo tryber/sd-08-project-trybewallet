96cb36a07b42e9afbac7499d6db8ecb968a5cf4d
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:28:31 -0300

    lets go again

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,22 +1,13 @@
-import { SAVE_EMAIL, SAVE_PASSWORD } from '../actions';
+import { USER_LOGIN } from '../actions';
 
-const LOGIN_INITIAL_STATE = {
+const INITIAL_STATE = {
   email: '',
-  password: '',
 };
 
-const userReducer = (state = LOGIN_INITIAL_STATE, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-  case SAVE_EMAIL:
-    return {
-      ...state, email: action.email,
-    };
-
-  case SAVE_PASSWORD:
-    return {
-      ...state, password: action.password,
-    };
-
+  case USER_LOGIN:
+    return { ...state, email: action.email };
   default:
     return state;
   }
