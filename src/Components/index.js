b5e665eed69bc8c4681756629a30ee594f303bda
Jorge Alves <alvesdjorge@gmail.com>
2021-03-12 16:44:23 -0300

    Components

---
+++ src/Components/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { user, totalExpenses } = this.props;
+    return (
+      <header>
+
+        <div>
+          <p data-testid="email-field">{ user }</p>
+          <div data-testid="total-field">{ totalExpenses }</div>
+          <div data-testid="header-currency-field">BRL</div>
+        </div>
+      </header>
+    );
+  }
+}
+Header.propTypes = {
+  user: PropTypes.string.isRequired,
+  totalExpenses: PropTypes.number.isRequired,
+};
+export default Header;
