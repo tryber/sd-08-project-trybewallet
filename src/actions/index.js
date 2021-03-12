1c39bc921534199e4564be272b4fc9bb8c6f84f4
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 08:32:18 -0300

    subindo tudo que havia feito e a complementação dps do plantao

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,51 @@
-// Coloque aqui suas actions
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.sumOftotalExpenses = this.sumOftotalExpenses.bind(this);
+  }
+
+  sumOftotalExpenses() {
+    const { expenses } = this.props;
+    const total = expenses.reduce((acc, current) => (
+      acc + current.value * current.exchangeRates[current.currency].ask
+    ), 0);
+
+    return total.toFixed(2);
+  }
+
+  render() {
+    const { email } = this.props;
+    return (
+      <div>
+        <p>
+          <strong>Email:</strong>
+          <span data-testid="email-field">{ email }</span>
+        </p>
+        <p>
+          <strong>Despesa Total:</strong>
+          <span data-testid="total-field">{ this.sumOftotalExpenses() }</span>
+          <strong>Câmbio:</strong>
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  expenses: state.wallet.expenses,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,25 @@
-// Esse reducer será responsável por tratar as informações da pessoa usuária
+import { SAVE_EMAIL, SAVE_PASSWORD } from '../actions';
+
+const LOGIN_INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+const userReducer = (state = LOGIN_INITIAL_STATE, action) => {
+  switch (action.type) {
+  case SAVE_EMAIL:
+    return {
+      ...state, email: action.email,
+    };
+
+  case SAVE_PASSWORD:
+    return {
+      ...state, password: action.password,
+    };
+
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
