11a2bbd789782d5000ba0d3e8b252c727280dd01
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:45:03 -0300

    store

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Login from './pages/Login';
-import Wallet from './pages/Wallet';
+import { Switch, Route } from 'react-router-dom';
+
+import Login from '../pages/Login';
+import Wallet from '../pages/Wallet';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route exact path="/carteira" component={ Wallet } />
+        <Route path="/carteira" component={ Wallet } />
       </Switch>
     );
   }
