e32fac190b7a56bc1cb8089ae014a1b789c0aece
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:58:38 -0300

    req fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+
+import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
