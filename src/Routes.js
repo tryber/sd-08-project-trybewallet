529f16b244f2e6c946741f0c686f8570f6dcc7b1
antonio arieiro <sfgfffs@gmail.com>
2021-03-12 09:47:07 -0300

    fix login and wallet

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Login from '../pages/Login';
-import Wallet from '../pages/Wallet';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 class Routes extends React.Component {
   render() {
