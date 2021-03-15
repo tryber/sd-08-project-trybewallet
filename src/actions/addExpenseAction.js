068633629bf5f7cfc24430bbcb91016f6014e870
Bardo <martins7ribeiro@gmail.com>
2021-03-14 22:14:16 -0300

    login gh-actions fix

--- src/actions/addExpenseAction.js
+++ src/actions/addExpenseAction.js
@@ -1,32 +1,32 @@
-import { ADD_EXPENSE } from './types';
+// import { ADD_EXPENSE } from './types';
 
-function addExpense(expense) {
-  return {
-    type: ADD_EXPENSE,
-    payload: expense,
-  };
-}
+// function addExpense(expense) {
+//   return {
+//     type: ADD_EXPENSE,
+//     payload: expense,
+//   };
+// }
 
-function addExpenseAction(expense) {
-  return (dispatch) => {
-    const endpoint = ('https://economia.awesomeapi.com.br/json/all');
-    const currentExchange = fetch(endpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        const currencies = Object.entries(data);
-        const filtered = currencies.filter((item) => item[0] !== 'USDT');
-        const exchanges = {};
-        filtered.forEach((item) => {
-          Object.assign(exchanges, { [item[0]]: item[1] });
-        });
-        const expenseToAdd = {
-          ...expense,
-          exchangeRates: exchanges,
-        };
-        dispatch(addExpense(expenseToAdd));
-      });
-    return currentExchange;
-  };
-}
+// function addExpenseAction(expense) {
+//   return (dispatch) => {
+//     const endpoint = ('https://economia.awesomeapi.com.br/json/all');
+//     const currentExchange = fetch(endpoint)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const currencies = Object.entries(data);
+//         const filtered = currencies.filter((item) => item[0] !== 'USDT');
+//         const exchanges = {};
+//         filtered.forEach((item) => {
+//           Object.assign(exchanges, { [item[0]]: item[1] });
+//         });
+//         const expenseToAdd = {
+//           ...expense,
+//           exchangeRates: exchanges,
+//         };
+//         dispatch(addExpense(expenseToAdd));
+//       });
+//     return currentExchange;
+//   };
+// }
 
-export default addExpenseAction;
+// export default addExpenseAction;
--- src/actions/currenciesFetchAction.js
+++ src/actions/currenciesFetchAction.js
@@ -1,24 +1,24 @@
-import { FETCH_CURRENCIES } from './types';
+// import { FETCH_CURRENCIES } from './types';
 
-function currenciesFromAPI(currencies) {
-  return {
-    type: FETCH_CURRENCIES,
-    payload: currencies,
-  };
-}
+// function currenciesFromAPI(currencies) {
+//   return {
+//     type: FETCH_CURRENCIES,
+//     payload: currencies,
+//   };
+// }
 
-function currenciesFetchAction() {
-  return (dispatch) => {
-    const endpoint = ('https://economia.awesomeapi.com.br/json/all');
-    const currencies = fetch(endpoint)
-      .then((response) => response.json())
-      .then((data) => {
-        const currenciesArray = Object.entries(data);
-        const filteredCurrencies = currenciesArray.filter((item) => item[0] !== 'USDT');
-        dispatch(currenciesFromAPI(filteredCurrencies));
-      });
-    return currencies;
-  };
-}
+// function currenciesFetchAction() {
+//   return (dispatch) => {
+//     const endpoint = ('https://economia.awesomeapi.com.br/json/all');
+//     const currencies = fetch(endpoint)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const currenciesArray = Object.entries(data);
+//         const filteredCurrencies = currenciesArray.filter((item) => item[0] !== 'USDT');
+//         dispatch(currenciesFromAPI(filteredCurrencies));
+//       });
+//     return currencies;
+//   };
+// }
 
-export default currenciesFetchAction;
+// export default currenciesFetchAction;
