8d03091d0fa5f0548aa5691eaf469ea2e74d51bc
victorjunior95 <victorjr.muchacho@gmail.com>
2021-03-18 18:52:00 -0300

    Requisito 1 completed

---
+++ src/components/LoginForm.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { saveEmail as saveEmailAction } from '../actions';
+
+// Salve o email no estado da aplicação, com a chave email, assim que a pessoa usuária logar.
+
+// A rota deve ser mudada para '/carteira' após o clique no botão 'Entrar'.
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      disabledEmail: true,
+      disabledPassword: true,
+    };
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    const { email } = this.state;
+    const { history, saveEmail } = this.props;
+    saveEmail(email);
+    history.push('/carteira');
+  }
+
+  handleChangeEmail(event) {
+    const email = event.target.value;
+
+    this.setState({
+      email: event.target.value,
+      disabledEmail: !this.emailValidate(email),
+    });
+  }
+
+  handleChangePassword(event) {
+    const password = event.target.value;
+
+    this.setState({
+      password: event.target.value,
+      disabledPassword: !this.passwordValidate(password),
+    });
+  }
+
+  emailValidate(email) {
+    const re = /^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$/; // plantão
+    return re.test(String(email).toLowerCase());
+  }
+
+  passwordValidate(password) {
+    const minCaracters = 5;
+    return password.length > minCaracters;
+  }
+
+  render() {
+    const { email, password, disabledEmail, disabledPassword } = this.state;
+
+    return (
+      <form action="">
+        <input
+          type="text"
+          name
+          data-testid="email-input"
+          value={ email }
+          onChange={ this.handleChangeEmail }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ this.handleChangePassword }
+        />
+        <button
+          type="button"
+          disabled={ disabledEmail || disabledPassword }
+          onClick={ this.onClick }
+        >
+          Entrar
+        </button>
+      </form>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+//   name: state.user.name,
+// });
+
+const mapDispatchToProps = (dispatch) => ({
+  saveEmail: (email) => dispatch(saveEmailAction(email)),
+});
+
+LoginForm.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(LoginForm);
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,9 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {};
+
+export default function wallet(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
