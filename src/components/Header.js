42f1d6e8971f1abfe693128e75a0afa2b9698567
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-03-18 19:06:06 -0300

    Correçoes no requisito 3

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ export default function Header({ email, total }) {
       <h1>TRYBE WALLET</h1>
       <div className="infos-header">
         <p data-testid="total-field">
-          <span data-testid="header-currency-field">R$</span>
-          {total === 0 ? total.toFixed(2) : 0}
+          <span data-testid="header-currency-field">BRL</span>
+          {total > 0 ? total.toFixed(2) : ''}
         </p>
-        <p data-testid="email-field">{ email }</p>
+        <p data-testid="email-field">{ email}</p>
       </div>
     </div>
   );
