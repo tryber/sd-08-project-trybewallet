00c0f51145c2401232eec700f8ce7295a84ab14f
Andre Sampaio <andresampaiolm@gmail.com>
2021-03-18 15:23:27 -0300

    delete button fixed

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { removeExpense } from '../actions';
 
 class ExpensesTable extends React.Component {
   constructor() {
