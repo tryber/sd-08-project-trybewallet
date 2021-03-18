ef0b7c74a0ff44f29cd8e9996625edae40a2933e
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-03-18 15:09:40 -0300

    Trybe Wallet

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchCurrencies as getCurrencies } from '../actions';
+import { ExpensesTable, Header, NewExpenseForm, EditExpenseForm } from '../components';
 
 class Wallet extends React.Component {
+  componentDidMount() {
+    const { fetchCurrencies } = this.props;
+    fetchCurrencies();
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { isEditing } = this.props;
+    return (
+      <main className="wallet-main">
+        <Header />
+        { isEditing ? <EditExpenseForm /> : <NewExpenseForm />}
+        <ExpensesTable />
+      </main>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  isEditing: state.wallet.isEditing,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchCurrencies: () => dispatch(getCurrencies()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
+
+Wallet.propTypes = {
+  isEditing: PropTypes.bool,
+  fetchCurrencies: PropTypes.func.isRequired,
+};
+
+Wallet.defaultProps = {
+  isEditing: false,
+};
