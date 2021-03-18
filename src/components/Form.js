e3c44d72d4bf8f85f23d2c12fa7450900ebeffc9
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-03-18 20:56:59 -0300

    Outra tentativa correçoes no requisito 3

--- src/components/Form.js
+++ src/components/Form.js
@@ -126,7 +126,7 @@ export default class Form extends Component {
               className="add-expense"
               onClick={ editSubmit }
             >
-              Editar
+              Editar despesa
             </button>
           )}
         </div>
