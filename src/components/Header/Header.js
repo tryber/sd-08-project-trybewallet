ed81b5eefa0d939b9cc68816934091efae9994b5
antonioarieiro <sfgfffs@gmail.com>
2021-03-19 09:47:21 -0300

    finnal push after help

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,51 +2,183 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-class Header extends React.Component {
-  render() {
-    const { user, expenses } = this.props;
+import { addExpense, submitEdit } from '../../actions';
 
-    const total = expenses.reduce(
-      (acc, expense) => (
-        acc + (parseInt(expense.value, 10)
-        * expense.exchangeRates[expense.currency].ask)
-      ), 0,
-    );
+const INITIAL_CLASS_STATE = {
+  value: '0',
+  description: '',
+  currency: 'USD',
+  payMethod: 'Dinheiro',
+  tag: 'Alimentação',
+};
+
+class ExpenseForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { ...INITIAL_CLASS_STATE, availableId: 0 };
+
+    this.addNewExpense = this.addNewExpense.bind(this);
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.createCurrencySelectionField = this.createCurrencySelectionField.bind(this);
+    this.editExistingExpense = this.editExistingExpense.bind(this);
+  }
+
+  onFieldChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  async addNewExpense() {
+    const { createExpense } = this.props;
+    const { currency, description, payMethod, tag, value, availableId } = this.state;
+    const currencies = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const exchangeRates = await currencies.json();
+
+    const expense = {
+      id: availableId,
+      value,
+      description,
+      currency,
+      method: payMethod,
+      tag,
+      exchangeRates,
+    };
+
+    createExpense(expense);
+
+    this.setState({ ...INITIAL_CLASS_STATE, availableId: availableId + 1 });
+  }
+
+  editExistingExpense() {
+    const { editExpense } = this.props;
+    const { currency, description, payMethod, tag, value, availableId } = this.state;
+
+    const expense = {
+      value,
+      description,
+      currency,
+      method: payMethod,
+      tag,
+    };
+
+    editExpense(expense);
+
+    this.setState({ ...INITIAL_CLASS_STATE, availableId: availableId + 1 });
+  }
+
+  createCurrencySelectionField() {
+    const { availableCurrencies } = this.props;
+    const { currency: currencyState } = this.state;
 
     return (
-      <header>
-        <p>
-          Usuário:
-          {' '}
-          <span data-testid="email-field">
-            {user}
-          </span>
-        </p>
-        <p>
-          Despesas: R$
-          <span data-testid="total-field">{total.toFixed(2)}</span>
-        </p>
-        <p>
-          Moeda de Câmbio:
-          {' '}
-          <span data-testid="header-currency-field">
-            BRL
-          </span>
-        </p>
-      </header>
+      <select
+        data-testid="currency-input"
+        name="currency"
+        value={ currencyState }
+        onChange={ this.onFieldChange }
+      >
+        {availableCurrencies.map((currency, index) => (
+          <option
+            key={ index }
+            data-testid={ currency }
+            value={ currency }
+          >
+            {currency}
+          </option>
+        ))}
+      </select>
+    );
+  }
+
+  createTags() {
+    return (
+      <>
+        <option value="Alimentação">Alimentação</option>
+        <option value="Lazer">Lazer</option>
+        <option value="Trabalho">Trabalho</option>
+        <option value="Transporte">Transporte</option>
+        <option value="Saúde">Saúde</option>
+      </>
+    );
+  }
+
+  createPaymentMethods() {
+    return (
+      <>
+        <option value="Dinheiro">Dinheiro</option>
+        <option value="Cartão de crédito">Cartão de crédito</option>
+        <option value="Cartão de débito">Cartão de débito</option>
+      </>
+    );
+  }
+
+  render() {
+    const { description, payMethod, tag, value } = this.state;
+    const { isEditing } = this.props;
+
+    return (
+      <form>
+        <input
+          type="number"
+          data-testid="value-input"
+          placeholder="Valor"
+          name="value"
+          onChange={ this.onFieldChange }
+          value={ value }
+        />
+        <input
+          type="text"
+          data-testid="description-input"
+          placeholder="Descrição"
+          name="description"
+          onChange={ this.onFieldChange }
+          value={ description }
+        />
+        {this.createCurrencySelectionField()}
+        <select
+          data-testid="method-input"
+          name="payMethod"
+          value={ payMethod }
+          onChange={ this.onFieldChange }
+        >
+          {this.createPaymentMethods()}
+        </select>
+        <select
+          data-testid="tag-input"
+          name="tag"
+          value={ tag }
+          onChange={ this.onFieldChange }
+        >
+          {this.createTags()}
+        </select>
+        <button
+          type="button"
+          onClick={ isEditing ? this.editExistingExpense : this.addNewExpense }
+        >
+          {isEditing ? 'Editar despesa' : 'Adicionar despesa'}
+        </button>
+      </form>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  expenses: state.wallet.expenses,
+  editingId: state.wallet.editingId,
+  isEditing: state.wallet.isEditing,
 });
 
-Header.propTypes = {
-  user: PropTypes.string.isRequired,
-  expenses: PropTypes.arrayOf(PropTypes.shape({
+const mapDispatchToProps = (dispatch) => ({
+  createExpense: (expense) => dispatch(addExpense(expense)),
+  editExpense: (expense) => dispatch(submitEdit(expense)),
+});
 
-  })).isRequired,
+ExpenseForm.propTypes = {
+  availableCurrencies: PropTypes.arrayOf(PropTypes.string).isRequired,
+  createExpense: PropTypes.func.isRequired,
+  editExpense: PropTypes.func.isRequired,
+  isEditing: PropTypes.bool.isRequired,
 };
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseForm);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,66 +2,98 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import Header from '../components/Header';
-import ExpenseForm from '../components/ExpenseForm';
-import CurrentExpenses from '../components/CurrentExpenses/index';
+import { Redirect } from 'react-router-dom';
+import { userLoginAction } from '../actions';
 
-import { fetchCurrencies as fetchCurrenciesAction } from '../actions';
+class Login extends React.Component {
+  constructor() {
+    super();
 
-class Wallet extends React.Component {
-  componentDidMount() {
-    const { fetchCurrencies } = this.props;
-    fetchCurrencies();
+    this.state = {
+      email: '',
+      password: '',
+      isButtonDisabled: true,
+      shouldRedirect: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.verifyFieldsValidation = this.verifyFieldsValidation.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  onInputChange({ target }) {
+    this.setState(
+      { [target.name]: target.value },
+      () => {
+        this.setState({ isButtonDisabled: !this.verifyFieldsValidation() });
+      },
+    );
+  }
+
+  onLogin(email) {
+    const { submitLogin } = this.props;
+    submitLogin(email);
+
+    this.setState({ shouldRedirect: true });
+  }
+
+  verifyFieldsValidation() {
+    const { email, password } = this.state;
+    const MINIMAL_PASSWORD_CHARS = 6;
+
+    const emailRegex = new RegExp([
+      '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)',
+      '|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])',
+      '|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$',
+    ].join(''));
+
+    const isEmailValid = emailRegex.test(email);
+    const isPasswordValid = password.length >= MINIMAL_PASSWORD_CHARS;
+
+    return isEmailValid && isPasswordValid;
   }
 
   render() {
-    const { userEmail, currencies, isLoading } = this.props;
+    const { email, password, isButtonDisabled, shouldRedirect } = this.state;
 
-    // if (!userEmail) return <Redirect to="/" />;
-
-    const availableCurrencies = currencies
-      ? currencies.map((currency) => currency.currency) : [];
+    if (shouldRedirect) return <Redirect to="/carteira" />;
 
     return (
       <>
-        <Header user={ userEmail } />
-        <main>
-          {
-            isLoading
-              ? <h1>Loading...</h1>
-              : (
-                <ExpenseForm
-                  availableCurrencies={ availableCurrencies }
-                />
-              )
-          }
-          <CurrentExpenses />
-        </main>
+        <input
+          type="text"
+          placeholder="Login"
+          name="email"
+          data-testid="email-input"
+          onChange={ this.onInputChange }
+          value={ email }
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          name="password"
+          data-testid="password-input"
+          onChange={ this.onInputChange }
+          value={ password }
+        />
+        <button
+          type="button"
+          disabled={ isButtonDisabled }
+          onClick={ () => this.onLogin(email) }
+        >
+          Entrar
+        </button>
       </>
     );
   }
 }
 
-const mapStateToProps = (state) => ({
-  userEmail: state.user.email,
-  expenses: state.wallet.expenses,
-  currencies: state.wallet.currencies,
-  isLoading: state.wallet.isLoading,
-});
-
 const mapDispatchToProps = (dispatch) => ({
-  fetchCurrencies: () => dispatch(fetchCurrenciesAction()),
+  submitLogin: (email) => dispatch(userLoginAction(email)),
 });
 
-Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-  currencies: PropTypes.arrayOf(
-    PropTypes.shape({
-      currency: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
-  fetchCurrencies: PropTypes.func.isRequired,
-  isLoading: PropTypes.bool.isRequired,
+Login.propTypes = {
+  submitLogin: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Wallet);
+export default connect(null, mapDispatchToProps)(Login);
