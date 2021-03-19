a3b0290f7d1eaa1302f4f984c9c73456d7773f89
Erick-Marinho <erickgaves@gmail.com>
2021-03-19 03:19:04 -0300

    corrigindo lint pela enesima vez

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/Store';
+import store from './store/Store.js';
 
 ReactDOM.render(
   <Provider store={ store }>
