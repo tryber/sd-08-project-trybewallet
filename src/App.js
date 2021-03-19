bd6b3c36c4841383e294f568eaca8d8bd31a629d
Erick-Marinho <erickgaves@gmail.com>
2021-03-19 03:09:10 -0300

    corrigindo lint mais uma vez

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-
 function App() {
   return (
     <Switch>
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
+import rootReducer from '../reducers/index';
 
-import rootReducer from '../reducers';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
