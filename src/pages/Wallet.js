a8e5e85b7c9f2365d667f84bf2c107d7f11d713e
paulohp7 <ph.pereira.7@gmail.com>
2021-03-18 22:00:08 -0300

    Fourth Objective

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,7 +3,50 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      moedas: '',
+      fetching: false,
+    };
+    this.renderCurrency = this.renderCurrency.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((response) => response.json())
+      .then((data) => {
+        const dataValues = Object.values(data).filter((moeda) => (
+          moeda !== 'USDT' && moeda.name !== 'Dólar Turismo'
+        ));
+        this.setState({ moedas: dataValues, fetching: true });
+      });
+  }
+
+  renderCurrency() {
+    const { moedas } = this.state;
+    return (
+      <div>
+        <label htmlFor="currency_input">
+          Escolha uma moeda:
+          <select id="currency" data-testid="currency-input">
+            {moedas.map((moeda, index) => (
+              <option
+                key={ index }
+                value={ moeda.bid }
+                data-testid={ moeda.code }
+              >
+                {moeda.code}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+    );
+  }
+
   render() {
+    const { fetching } = this.state;
     const { userEmail } = this.props;
     return (
       <div>
@@ -13,6 +56,26 @@ class Wallet extends React.Component {
         <br />
         <p data-testid="total-field">0</p>
         <p data-testid="header-currency-field">BRL</p>
+        <label htmlFor="value_input">
+          Despesa:
+          <input
+            id="value_input"
+            type="text"
+            name="value"
+            placeholder="Digite a despesa"
+            data-testid="value-input"
+          />
+        </label>
+        <label htmlFor="description_input">
+          Descrição:
+          <input
+            id="description_input"
+            type="text"
+            name="description"
+            data-testid="description-input"
+          />
+        </label>
+        {fetching ? this.renderCurrency() : <div>Loading...</div>}
       </div>
     );
   }
