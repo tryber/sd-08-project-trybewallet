8996b5b8ac3ab96730579a3c8fcd9d46918cdaa6
Erick-Marinho <erickgaves@gmail.com>
2021-03-19 03:21:48 -0300

    novamente o lint que não quer passar

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store/Store.js';
+import store from './store/store';
 
 ReactDOM.render(
   <Provider store={ store }>
