9a44edc3fe4e8d366a31f3ff1ba88aca4c927f97
Erick-Marinho <erickgaves@gmail.com>
2021-04-21 23:15:46 -0300

    combined reducers

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
-import user from '../reducers/user';
+import rootReducer from '../reducers/index';
 
-const store = createStore(user, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
