8ded1d5986e77ca8cbdd8497e5fc22b16c618380
Erick-Marinho <erickgaves@gmail.com>
2021-04-21 23:03:30 -0300

    implementing devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
 import user from '../reducers/user';
 
-const store = createStore(user);
+const store = createStore(user, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
